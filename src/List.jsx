3cd06027c038637a8e31bd1cbdd4a34754157059
Tim <Ti.corwin229@gmail.com>
2023-11-03 11:21:20 -0400

    first commit

---
+++ src/List.jsx
@@ -0,0 +1,13 @@
+import { Person } from "./Person";
+
+export const List = ({ people }) => {
+  return (
+    <section>
+      {people.map((person) => {
+        return <Person key={person.id} {...person} />;
+      })}
+    </section>
+  );
+};
+
+export default List;
---
+++ src/Person.jsx
@@ -0,0 +1,11 @@
+export const Person = ({ image, name, age }) => {
+  return (
+    <article className="person">
+      <img src={image} alt={name} className="img" />
+      <div>
+        <h4>{name}</h4>
+        <p>{age}</p>
+      </div>
+    </article>
+  );
+};
